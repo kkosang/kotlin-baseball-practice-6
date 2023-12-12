1e2fbb65e4b06fe74ac17e5fa90489d77b23296a
kkosang <87695921+kkosang@users.noreply.github.com>
2023-12-12 19:08:41 +0900

    feat: 예측숫자 입력받기

--- src/main/kotlin/baseball/controller/BaseballController.kt
+++ src/main/kotlin/baseball/controller/BaseballController.kt
@@ -1,11 +1,19 @@
 package baseball.controller
 
+import baseball.view.InputView
 import baseball.view.OutputView
 
 class BaseballController {
     private val outputView = OutputView()
+    private val inputView = InputView()
 
     fun gameStart() {
         outputView.printStartMessage()
+        inputPredictionNumber()
+    }
+
+    fun inputPredictionNumber() {
+        val input = inputView.printPredictionNumber()
+        
     }
 }
--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,4 +1,13 @@
 package baseball.view
 
+import INPUT_PREDICTION_NUMBER
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    fun printPredictionNumber(): String {
+        print(INPUT_PREDICTION_NUMBER)
+        val predictionNumber = Console.readLine()
+
+        return predictionNumber
+    }
 }
