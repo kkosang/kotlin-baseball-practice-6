9edc639a534f12d724ef6e8ec5d28b8cc5030869
kkosang <87695921+kkosang@users.noreply.github.com>
2023-12-12 19:20:50 +0900

    feat: 예측숫자 숫자인지 검증

--- src/main/kotlin/baseball/domain/PredictionNumber.kt
+++ src/main/kotlin/baseball/domain/PredictionNumber.kt
@@ -3,10 +3,18 @@ package baseball.domain
 class PredictionNumber(private val number: String) {
     fun validate() {
         isLengthThree()
+        isNumeric()
     }
 
     private fun isLengthThree() {
         if (number.length != 3) {
+            callException()
+        }
+    }
+
+    private fun isNumeric() {
+        val num = number.toIntOrNull()
+        if (num == null) {
             callException()
         }
     }
