2e67dc40234c4527a3c72fc2e3d08ec8966c94e5
kkosang <87695921+kkosang@users.noreply.github.com>
2023-12-12 18:57:33 +0900

    feat: 메시지 상수화

---
+++ src/main/kotlin/baseball/util/Message.kt
@@ -0,0 +1,10 @@
+val START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다."
+val FINISH_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+val RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+val INPUT_PREDICTION_NUMBER = "숫자를 입력해주세요 : "
+val BALL = "볼"
+val STRIKE = "볼"
+
+//exception
+val ERROR_MESSAGE = "유효하지 않은 입력입니다."
+
